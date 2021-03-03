ae1c3cd6a5603a8e75146f11c9e4715f4098ac58
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 20:15:16 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -9,3 +9,22 @@ end
 
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
+end
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
+end
